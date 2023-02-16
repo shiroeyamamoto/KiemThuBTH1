7fe7687430b027543fb748f6c989004dd1848479
shiroeyamamoto <khangvngm@gmail.com>
2023-02-16 14:41:51 +0700

    update phu co quan

--- MyApp/MyApp/Form1.cs
+++ MyApp/MyApp/Form1.cs
@@ -16,5 +16,10 @@ namespace MyApp
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
